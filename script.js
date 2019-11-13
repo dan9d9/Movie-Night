661848cd52dd0e313bae20552d69ce6494baaa72
Daniel Dick <dan9d9@gmail.com>
2019-11-13 11:41:45 +0100

    add contenteditable property to list item

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ function createList(array = [], list) {
 
 	list.innerHTML = array.map((movie) => {
 		return `
-			<li class='itemClass' data-list=${list.id} data-index=${movie.index}>
+			<li class='itemClass' contenteditable="true" data-list=${list.id} data-index=${movie.index}>
 				${movie.title}
 				<button class='btnApprove' data-index=${movie.index}>\u2713</button>
 				<button class='btnDelete' data-index=${movie.index}>\u2717</button>
