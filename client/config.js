7f17063b640ef40c5901c74dc0f98133b9e3099d
Daniel Dick <dan9d9@gmail.com>
2020-05-28 10:55:58 +0200

    type on module exports

--- client/config.js
+++ client/config.js
@@ -2,4 +2,4 @@ const URL = window.location.hostname === `localhost`
             ? `http://localhost:5000`
             : `http://167.172.102.224`;
 
-modules.export = { URL }
+module.exports = { URL }
