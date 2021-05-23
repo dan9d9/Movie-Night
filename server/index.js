b2b6029c9413ec17c00e44cea4ab7b535c30b3ae
Daniel Dick <dan9d9@gmail.com>
2021-05-23 11:25:03 +0200

    remove body-parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const path = require('path');
 const app = express();
@@ -11,8 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
 
 //================ CORS ================================
 const cors = require('cors');
